2d4b0ee9b5c1c5112c6668c3ef9b2bf54fd73923
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-06-10 19:19:12 -0300

    Create Hero.js

---
+++ src/Components/Hero.js
@@ -0,0 +1,29 @@
+/*
+  {
+    dateFrom: new Date(),
+    dateTo: new Date(),
+    country: undefined,
+    price: undefined,
+    rooms: undefined
+  }
+*/
+import React from 'react';
+class Hero extends React.Component
+{
+  render()
+  {
+    return (
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Hoteles</h1>
+            <h2 className="subtitle">
+              desde el <strong>dddd, DD de mmmm de AAAA</strong> hasta el <strong>dddd, DD de mmmm de AAAA</strong>
+            </h2>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+export default Boton;
