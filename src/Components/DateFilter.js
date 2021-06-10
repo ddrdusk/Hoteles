9afa73d4891e4fb7455707f043a8929e7d0ee6bd
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-06-10 19:21:44 -0300

    Create DateFilter.js

---
+++ src/Components/DateFilter.js
@@ -0,0 +1,27 @@
+/*
+  {
+    dateFrom: new Date(),
+    dateTo: new Date(),
+    country: undefined,
+    price: undefined,
+    rooms: undefined
+  }
+*/
+import React from 'react';
+class Hero extends React.Component
+{
+  render()
+  {
+    return (
+      <div className="field">
+        <div className="control has-icons-left">
+          <input className="input" type="date" />
+          <span className="icon is-small is-left">
+            <i className="fas"></i>
+          </span>
+        </div>
+      </div>
+    )
+  }
+}
+export default Boton;
